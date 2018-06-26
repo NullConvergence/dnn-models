e87dbcf9665b851d9b1bf90fe8b202eb35db13ea
NullConvergence <a.serban@cs.ru.nl>
2018-06-26 22:07:42 +0200

    Updated package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name='dnn-models',
+setup(name='dnn_models',
       version='1.0.0',
       url='https://github.com/NullConvergence/dnn-models',
       license='MIT',
