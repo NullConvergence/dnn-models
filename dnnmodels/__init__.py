3e86c088f9f978d7e75429be5a28490549b2f79c
NullConvergence <a.serban@cs.ru.nl>
2018-06-26 22:22:54 +0200

    Updated init and setup + added LICENSE

--- dnnmodels/__init__.py
+++ dnnmodels/__init__.py
@@ -2,3 +2,4 @@ import os
 import pkgutil
 __all__ = list(module for _, module, _ in pkgutil.iter_modules(
     [os.path.dirname(__file__)]))
+name = "dnnmodels"
--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='dnnmodels',
       version='1.0.0',
       url='https://github.com/NullConvergence/dnnmodels',
       license='MIT',
       author='NullConvergence',
+      description="Simple models inspired by Cleverhans that help create DNN Architectures",
       install_requires=[
           'cleverhans',
           'tensorflow'
       ],
-      packages=['deepmodels'])
+      packages=find_packages())
