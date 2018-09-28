1a27e10a7335b123d9bf774d3130223898cec416
NullConvergence <a.serban@cs.ru.nl>
2018-09-28 21:58:46 +0200

    Initial commit with data module

---
+++ dnnmodels/data/data.py
@@ -0,0 +1,11 @@
+### 
+# Serves as an interface for getting train data
+# Any pre-processing steps will be implemented in 
+# the individual data files. e.g. mnist.py
+###
+
+def get_mnist():
+    pass
+
+def get_imagenet():
+    pass
